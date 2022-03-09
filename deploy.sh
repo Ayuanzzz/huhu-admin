0df4f036dfb93cff0a25bf13dc5ba072cfcd512a
Ayuan <8679461+sugarpeter@user.noreply.gitee.com>
2022-03-09 17:02:20 +0800

    部署测试

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-如果你要部署在 https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Ayuanzzz/huhu-admin.git main:gh-pages
+如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Ayuanzzz/huhu-admin.git main:gh-pages
 
 cd -
